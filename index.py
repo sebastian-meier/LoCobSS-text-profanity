1f0c11b52ba8154b61050649cfd40d1df6f427f9
Sebastian Meier <contact@sebastianmeier.eu>
2020-12-03 10:36:11 +0100

    research

---
+++ index.py
@@ -0,0 +1,17 @@
+https://pypi.org/project/profanity-filter/
+
+https://www.npmjs.com/package/bad-words
+https://github.com/thisandagain/washyourmouthoutwithsoap
+https://github.com/d-oliveros/profanity-censor
+https://towardsdatascience.com/building-a-better-profanity-detection-library-with-scikit-learn-3638b2f2c4c2
+https://github.com/vzhou842/profanity-check
+https://github.com/areebbeigh/profanityfilter
+https://stackoverflow.com/questions/273516/how-do-you-implement-a-good-profanity-filter
+https://www.npmjs.com/package/bad-words
+https://hatebase.org/about
+https://www.freewebheaders.com/full-list-of-bad-words-banned-by-google/
+https://paperswithcode.com/task/hate-speech-detection
+https://github.com/Hironsan/HateSonar
+https://github.com/NakulLakhotia/Hate-Speech-Detection-in-Social-Media-using-Python
+https://github.com/t-davidson/hate-speech-and-offensive-language
+https://arxiv.org/abs/1905.12516
