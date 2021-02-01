afa883eaf4cfd1032162a81016352dc4e5650792
Sebastian Meier <contact@sebastianmeier.eu>
2021-02-01 18:26:45 +0100

    cloud run tests

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ class Predict(Resource):
 API.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-  APP.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 8080))
+  APP.run(debug=True, port=os.environ.get('PORT', 8080))
